400de24f96376f6294a62057529bf64b60903d33
sydneydenae <sydney.stokes@bison.howard.edu>
2023-02-05 11:27:43 -0500

    Added files for assignment2

---
+++ large-scale-programming/src/org/howard/edu/lsp/assignment2/Assignment2.java
@@ -0,0 +1,13 @@
+/**
+ * Sydney Stokes
+ */
+package org.howard.edu.lsp.assignment2;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+	}
+
+}
