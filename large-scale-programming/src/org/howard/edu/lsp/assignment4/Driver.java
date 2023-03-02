9503aa5ae9081af55cb7362c70b1051372863201
sydneydenae <sydney.stokes@bison.howard.edu>
2023-03-02 10:30:26 -0500

    Completed about half of the functions

--- large-scale-programming/src/org/howard/edu/lsp/assignment4/Driver.java
+++ large-scale-programming/src/org/howard/edu/lsp/assignment4/Driver.java
@@ -6,8 +6,33 @@ package org.howard.edu.lsp.assignment4;
 
 public class Driver {	
 	public static void main(String[] args) {
+		
+		IntegerSet set1 = new IntegerSet();
+		set1.add(1);
+		set1.add(2);
+		set1.add(2);
+		set1.add(7);
+		System.out.println("Value of Set1 is: " + set1.toString());
+		System.out.println("Length of Set1 is: " + set1.length());
+		System.out.println("Set 1 contains 2: " + set1.contains(2));
+		System.out.println("Largest value in Set1 is: " + set1.largest());
+		System.out.println("Smallest value in Set1 is: " + set1.smallest());
+
+		
+		IntegerSet set2 = new IntegerSet();
+		set2.add(1);
+		set2.add(2);
+		set2.add(7);
+		System.out.println("Value of Set2 is: " + set2.toString());
+		System.out.println("Set1 and Set2 are equal: " + set2.equals(set1));
+		
+		set1.clear();
+		System.out.println("Value of Set1 is: " + set1.toString());
+
+
+		
+		
 		/*
-		 * 
 		Make this to test IntegerSet
 		IntegerSet set1 = new IntegerSet();
 		set1.add(1);
--- large-scale-programming/src/org/howard/edu/lsp/assignment4/IntegerSet.java
+++ large-scale-programming/src/org/howard/edu/lsp/assignment4/IntegerSet.java
@@ -4,6 +4,7 @@
 
 package org.howard.edu.lsp.assignment4;
 
+import java.util.Collections;
 import java.util.HashSet;
 
 public class IntegerSet  {
@@ -29,9 +30,52 @@ public class IntegerSet  {
 	* Two sets are equal if they contain all of the same values in ANY order.
 	*/
 	public boolean equals(IntegerSet b) {
-		//NOT FINISHED
-		return true;
+		if(nums.equals(b.nums)) {
+			return true;
+		} else {
+			return false;
+		}
 	}; 
+	
+	//Returns true if the HashSet contains the value v
+	public boolean contains(int v) {
+		if(nums.contains(v)) {
+			return true;
+		} else {
+			return false;
+		}
+	}; 
+	
+	public int largest(){
+		//idk if this is the right way to call length
+		if (length() == 0) {
+			return -1;
+		} else {
+			return Collections.max(nums);
+		}
+	}; 
+	
+	public int smallest(){
+		//idk if this is the right way to call length
+		if (length() == 0) {
+			return -1;
+		} else {
+			return Collections.min(nums);
+		}
+	}; 
+	
+	public void add(int a) {
+		nums.add(a);
+	};
+	
+	public String toString() {
+		String printedNums = "";
+		for(int i: nums) {
+			printedNums += i;
+			printedNums += " ";
+			}
+		return printedNums;
+	};
 
 	
 	
